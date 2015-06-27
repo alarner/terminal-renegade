37264f0918220b34388f3460af8752ec25ac688e
Erika Moller <erikamoller@gmail.com>
2015-06-27 18:43:13 -0500

    added jquery and typed.js pckgs

--- src/scripts/components/StoryPage.js
+++ src/scripts/components/StoryPage.js
@@ -1,9 +1,18 @@
 var React = require('react');
+// var $ = require('jquery');
 
 module.exports = React.createClass({
+	componentDidMount: function() {
+	 	$('#typed').typed({
+	            strings: ["This is where the story goes. It will explain the game. Have fun!"],
+	            typeSpeed: 50,
+	            loop: false,
+	            loopCount: false
+	    });
+	},
 	render: function() {
 		return (
-			<section>
+			<section id="typed">
 				<a href="#game">Play Game</a>
 			</section>
 		);
