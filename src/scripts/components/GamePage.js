0a807a953169663aeffaa4804fc5a11d5eaed8bd
Aaron Larner <anlarner@gmail.com>
2015-06-27 12:46:17 -0500

    Build out page structure

---
+++ src/scripts/components/GamePage.js
@@ -0,0 +1,155 @@
+var React = require('react');
+var PIXI = require('pixi.js');
+var globals = require('../globals');
+
+var levels = [
+	require('../maps/001_tutorial'),
+	require('../maps/002_pilot_deck'),
+	require('../maps/003_sewage_pipes'),
+	require('../maps/004_biodome'),
+	require('../maps/005_experimental_facility'),
+	require('../maps/006_outer_space')
+];
+
+module.exports = React.createClass({
+	getInitialState: function() {
+		var level = {
+			renderer: new PIXI.WebGLRenderer(800, 600),
+			viewport: new PIXI.Container(),
+			stage: new PIXI.Container()
+		};
+		var home = {
+			renderer: new PIXI.WebGLRenderer(800, 600),
+			stage: new PIXI.Container()
+		};
+
+		level.viewport.addChild(level.stage);
+
+		return {
+			page: 'home',
+			level: level,
+			home: home
+		}
+	},
+	componentDidMount: function() {
+		this.refs.pixiHome.getDOMNode().appendChild(
+			this.state.home.renderer.view
+		);
+		this.refs.pixiLevel.getDOMNode().appendChild(
+			this.state.level.renderer.view
+		);
+		this.animate();
+	},
+	render: function() {
+		var homeStyle = {
+			display: this.state.page === 'home' ? 'block' :'none'
+		};
+		var levelStyle = {
+			display: this.state.page === 'level' ? 'block' :'none'
+		};
+		return (
+			<section>
+				<div style={homeStyle}>
+					<div ref="pixiHome"></div>
+					<button onClick={this.gotoLevel(0)}>Level 1</button>
+					<button onClick={this.gotoLevel(1)}>Level 2</button>
+					<button onClick={this.gotoLevel(2)}>Level 3</button>
+					<button onClick={this.gotoLevel(3)}>Level 4</button>
+					<button onClick={this.gotoLevel(4)}>Level 5</button>
+				</div>
+				<div style={levelStyle}>
+					<div ref="pixiLevel"></div>
+				</div>
+			</section>
+		);
+	},
+	gotoLevel: function(level) {
+		var self = this;
+		return function(e) {
+			console.log(levels[level]);
+			self.drawNode(levels[level].root, levels[level].display);
+			self.setState({page: 'level'});
+		};
+	},
+	getNodeWidth: function(node, pixels) {
+		var width = 0;
+		if(node._width) {
+			width = node._width;
+		}
+		else {
+			if(!node.children.length) {
+				width = 1;
+			}
+			else {
+				for(var i=0; i<node.children.length; i++) {
+					width += this.getNodeWidth(node.children[i]);
+				}
+			}
+			node._width = width;
+		}
+		if(pixels) {
+			return width*globals.node.size.width + (width-1)*globals.node.spacing.x;
+		}
+		return width;
+	},
+	getNodeHeight: function(node, pixels) {
+		var height = 0;
+		if(node._height) {
+			height =  node._height;
+		}
+		else {
+			if(!node.children.length) {
+				height = 1;
+			}
+			else {
+				var maxHeight = 0;
+				for(var i=0; i<node.children.length; i++) {
+					var tmp = this.getNodeHeight(node.children[i]);
+					if(tmp > maxHeight) maxHeight = tmp;
+				}
+				height = 1+maxHeight;
+			}
+			node._height = height;
+		}
+		if(pixels) {
+			return height*globals.node.size.height + (height-1)*globals.node.spacing.y;
+		}
+		return height;
+	},
+	drawNode: function(node, defaultDisplay, x, y) {
+		var width = this.getNodeWidth(node, true);
+		var height = this.getNodeHeight(node, true);
+
+		x = x || 0;
+		y = y || 0;
+
+		var draw = node.display ? node.display() : defaultDisplay();
+		draw.position.x = x + (width - draw.width)/2;
+		draw.position.y = y;
+
+		this.state.level.stage.addChild(draw);
+
+		var newY = y+globals.node.size.height+globals.node.spacing.y;
+		var previousNode = null;
+		for(var i=0; i<node.children.length; i++) {
+			var newX = x;
+			if(previousNode) {
+				newX += this.getNodeWidth(previousNode, true) + globals.node.spacing.x;
+			}
+			this.drawNode(node.children[i], defaultDisplay, newX, newY);
+			previousNode = node.children[i];
+		}
+	},
+	animate: function() {
+		// start the timer for the next animation loop
+		requestAnimationFrame(this.animate);
+
+		// this is the main render call that makes pixi draw your container and its children.
+		if(this.state.page === 'home') {
+			this.state.home.renderer.render(this.state.home.stage);
+		}
+		else if(this.state.page === 'level') {
+			this.state.level.renderer.render(this.state.level.viewport);
+		}
+	}
+});
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,56 +1,61 @@
-// External Libraries
-var PIXI = require('pixi.js');
-var level1 = require('./maps/001_tutorial.js');
-var globals = require('./globals');
-var Game = require('./game');
+var React = require('react');
+var App = require('./components/App');
 
-// Global Variables
-var renderer = new PIXI.WebGLRenderer(800, 600);
-var viewport = new PIXI.Container();
-var stage = new PIXI.Container();
+React.render(<App />, document.getElementById('container'));
 
-// Initial Setup
-document.body.appendChild(renderer.view);
-viewport.addChild(stage);
-var game = new Game(viewport, stage);
-game.startLevel(level1);
- // kick off the animation loop (defined below)
+// // External Libraries
+// var PIXI = require('pixi.js');
+// var level1 = require('./maps/001_tutorial.js');
+// var globals = require('./globals');
+// var Game = require('./game');
 
-var texture = PIXI.Texture.fromImage("../images/bunny.gif");
-    // create a new Sprite using the texture
-var bunny = new PIXI.Sprite(texture);
+// // Global Variables
+// var renderer = new PIXI.WebGLRenderer(800, 600);
+// var viewport = new PIXI.Container();
+// var stage = new PIXI.Container();
 
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
+// // Initial Setup
+// document.body.appendChild(renderer.view);
+// viewport.addChild(stage);
+// var game = new Game(viewport, stage);
+// game.startLevel(level1);
+//  // kick off the animation loop (defined below)
 
-// move the sprite t the center of the screen
-bunny.position.x = 200;
-bunny.position.y = 150;
+// var texture = PIXI.Texture.fromImage("../images/bunny.gif");
+//     // create a new Sprite using the texture
+// var bunny = new PIXI.Sprite(texture);
+
+// bunny.anchor.x = 0.5;
+// bunny.anchor.y = 0.5;
+
+// // move the sprite t the center of the screen
+// bunny.position.x = 200;
+// bunny.position.y = 150;
 
 
-bunny.scale.x = 0.5;
-bunny.scale.y = 0.5;
+// bunny.scale.x = 0.5;
+// bunny.scale.y = 0.5;
 
 
-stage.addChild(bunny);
+// stage.addChild(bunny);
 
-animate();
+// animate();
 
-function animate() {
-	// start the timer for the next animation loop
-	requestAnimationFrame(animate);
+// function animate() {
+// 	// start the timer for the next animation loop
+// 	requestAnimationFrame(animate);
 
-	if(bunny.position.x < 500){
- 		bunny.position.x += 2;
- 		bunny.position.y += 2;
- 	} else if(bunny.position.x > 502){
- 		bunny.position.x -= 2;
- 		bunny.position.y -= 2;
- 	}
- 	//bunny.acceleration.x += 4;
-        // render the stage   
-     renderer.render(stage);
+// 	if(bunny.position.x < 500){
+//  		bunny.position.x += 2;
+//  		bunny.position.y += 2;
+//  	} else if(bunny.position.x > 502){
+//  		bunny.position.x -= 2;
+//  		bunny.position.y -= 2;
+//  	}
+//  	//bunny.acceleration.x += 4;
+//         // render the stage   
+//      renderer.render(stage);
 	
-	// this is the main render call that makes pixi draw your container and its children.
-	renderer.render(viewport);
-}
+// 	// this is the main render call that makes pixi draw your container and its children.
+// 	renderer.render(viewport);
+// }
