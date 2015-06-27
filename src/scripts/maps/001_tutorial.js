8876b2cc3eacc621c823c7321913dc9047a42519
allen wes <allen.wes.g@gmail.com>
2015-06-27 15:18:50 -0500

    changed bunny to sprite

--- src/scripts/maps/001_tutorial.js
+++ src/scripts/maps/001_tutorial.js
@@ -9,32 +9,33 @@ module.exports = {
 		// // draw a rectangle
 		// graphics.drawRect(0, 0, globals.node.size.width-5, globals.node.size.height-5);
 		// return graphics;
-		var texture = PIXI.Texture.fromImage("../images/bunny.gif");
+		var texture = PIXI.Texture.fromImage("../images/active_node.png");
 		//var paranoiaTexture = PIXI.Texture.fromImage('../images/paranoia.png');
 		    // create a new Sprite using the texture
-		var bunny = new PIXI.Sprite(texture);
+		var sprite = new PIXI.Sprite(texture);
 		//var paranoia = new PIXI.Sprite(paranoiaTexture);
 
-		bunny.anchor.x = 0.5;
-		bunny.anchor.y = 0.5;
+		// sprite.anchor.x = 0.;
+		// sprite.anchor.y = 0.5;
 
 		// paranoia.anchor.x = 0.5;
 		// paranoia.anchor.y = 0.5;
 
-		// move the sprite to the center of the screen
-		bunny.position.x = 200;
-		bunny.position.y = 200;
+		// // move the sprite to the center of the screen
+		// sprite.position.x = 200;
+		// sprite.position.y = 200;
 
 		// paranoia.position.x = 200;
 		// paranoia.position.y = 200;
 
 
-		bunny.scale.x = 0.5;
-		bunny.scale.y = 0.5;
+		sprite.width = globals.node.size.width;
+		sprite.height = globals.node.size.height;
 
-		// paranoia.scale.x = 0.2;
-		// paranoia.scale.y = 0.2;
-		return bunny
+		// sprite.scale.x = 0.2;
+		// sprite.scale.y = 0.2; 
+
+		return sprite
 	},
 	root:{
 		name: '/',
@@ -72,15 +73,15 @@ module.exports = {
 				// var paranoiaTexture = PIXI.Texture.fromImage('../images/paranoia.png');
     // 			var paranoia = new PIXI.Sprite(paranoiaTexture);
 
-				display: function() {
-					var graphics = new PIXI.Graphics();
-					graphics.beginFill(0xFF00FF);
-					// set the line style to have a width of 5 and set the color to red
-					graphics.lineStyle(5, 0xFF0000);
-					// draw a rectangle
-					graphics.drawRect(0, 0, globals.node.size.width-5, globals.node.size.height-5);
-					return graphics;
-				},
+				// display: function() {
+				// 	var graphics = new PIXI.Graphics();
+				// 	graphics.beginFill(0xFF00FF);
+				// 	// set the line style to have a width of 5 and set the color to red
+				// 	graphics.lineStyle(5, 0xFF0000);
+				// 	// draw a rectangle
+				// 	graphics.drawRect(0, 0, globals.node.size.width-5, globals.node.size.height-5);
+				// 	return graphics;
+				// },
 				items: [
 				],
 				children: [
