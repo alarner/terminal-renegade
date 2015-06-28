8468c507b516407408555b4bc8fb9434a6c6e1bb
Aaron Larner <anlarner@gmail.com>
2015-06-27 19:44:16 -0500

    Add file graphics

--- src/scripts/maps/001_tutorial.js
+++ src/scripts/maps/001_tutorial.js
@@ -2,48 +2,29 @@ var PIXI = require('pixi.js');
 var globals = require('../globals');
 module.exports = {
 	display: function() {
-		// console.log('display', this);
-		
-		// var graphics = new PIXI.Graphics();
-		// graphics.beginFill(0xFFFF00);
-		// // set the line style to have a width of 5 and set the color to red
-		// graphics.lineStyle(5, 0xFF0000);
-		// // draw a rectangle
-		// graphics.drawRect(0, 0, globals.node.size.width-5, globals.node.size.height-5);
-		// return graphics;
 		var texture;
 
 		if(this.created) {
-			texture = PIXI.Texture.fromImage("../images/active_node.png");
+			if(this.type === 'file') {
+				texture = PIXI.Texture.fromImage("../images/file_active.png");
+			}
+			else if(this.type === 'directory') {
+				texture = PIXI.Texture.fromImage("../images/active_node.png");
+			}
 		}
-		else{
-			texture = PIXI.Texture.fromImage("../images/inactive_node.png");
+		else {
+			if(this.type === 'file') {
+				texture = PIXI.Texture.fromImage("../images/file_inactive.png");
+			}
+			else if(this.type === 'directory') {
+				texture = PIXI.Texture.fromImage("../images/inactive_node.png");
+			}
 		}
-
-		//var paranoiaTexture = PIXI.Texture.fromImage('../images/paranoia.png');
-		    // create a new Sprite using the texture
 		var sprite = new PIXI.Sprite(texture);
-		//var paranoia = new PIXI.Sprite(paranoiaTexture);
-
 		sprite.anchor.x = 0.5;
 		sprite.anchor.y = 0.5;
-
-		// paranoia.anchor.x = 0.5;
-		// paranoia.anchor.y = 0.5;
-
-		// // move the sprite to the center of the screen
-		// sprite.position.x = 200;
-		// sprite.position.y = 200;
-
-		// paranoia.position.x = 200;
-		// paranoia.position.y = 200;
-
-
-		sprite.width = globals.node.size.width;
-		sprite.height = globals.node.size.height;
-
-		// sprite.scale.x = 0.2;
-		// sprite.scale.y = 0.2; 
+		// sprite.width = globals.node.size.width;
+		// sprite.height = globals.node.size.height;
 
 		return sprite
 	},
