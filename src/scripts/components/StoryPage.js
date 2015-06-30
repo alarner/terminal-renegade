3b04111a158f48c6a63e14b463e5172a91e69940
Aaron Larner <anlarner@gmail.com>
2015-06-29 19:13:26 -0500

    Add typing animations to story page and clean up package.json

--- src/scripts/components/StoryPage.js
+++ src/scripts/components/StoryPage.js
@@ -5,12 +5,12 @@ require('typed.js');
 
 module.exports = React.createClass({
 	componentDidMount: function() {
-		// $('#typed').typed({
-		// 		strings: ["This is where the story goes. It will explain the game. Have fun!"],
-		// 		typeSpeed: 50,
-		// 		loop: false,
-		// 		loopCount: false
-		// });
+		$('#typed').typed({
+				strings: ["This is where the story goes. It will explain the game. Have fun!"],
+				typeSpeed: 50,
+				loop: false,
+				loopCount: false
+		});
 	},
 	render: function() {
 		return (
